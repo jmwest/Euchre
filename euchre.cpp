b9737eb06b6951f83dd12896df25a7d38409a4d0
JohnWest <johnwest@umich.edu>
2013-10-09 12:29:00 -0400

    Added a euchre file

---
+++ euchre.cpp
@@ -0,0 +1,9 @@
+//
+//  euchre.cpp
+//  proj3
+//
+//  Created by John West on 10/7/13.
+//  Copyright (c) 2013 John West. All rights reserved.
+//
+
+#include "euchre.h"
---
+++ euchre.h
@@ -0,0 +1,14 @@
+//
+//  euchre.h
+//  proj3
+//
+//  Created by John West on 10/7/13.
+//  Copyright (c) 2013 John West. All rights reserved.
+//
+
+#ifndef __proj3__euchre__
+#define __proj3__euchre__
+
+#include <iostream>
+
+#endif /* defined(__proj3__euchre__) */
